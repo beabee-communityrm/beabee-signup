e9ada8887ef99e83f8ab310c9e06cdc2345bfe62
Will Franklin <will@willpf.co.uk>
2022-12-07 15:51:44 +0000

    feat: add iframe resizer

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,8 @@ import router from './router';
 import icons from './lib/icons';
 import './index.css';
 
+import 'iframe-resizer/js/iframeResizer.contentWindow';
+
 const app = createApp(App);
 app.use(router);
 app.use(icons);
